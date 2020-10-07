677b5daeed0a79adc7bf577604f29808562e9cc8
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-07 12:10:34 -0700

    Update task1.py

--- task1.py
+++ task1.py
@@ -5,11 +5,14 @@ def sum(a,b):
     # a : float 
     # b : float
     # return value: returns the sum of the 2 numbers
+    answer= float(a)+float(b)
+    return answer
 
 
 
 #this should return a value of 7
 x = sum(3,4)
-
+print(x)
 #this should return a value of 12.5
 y = sum(11,1.5)
+print(y)
