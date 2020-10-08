b473ae4693f07cbe181fabd69f36b6e1d35fdc5a
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-08 12:44:08 -0700

    Update problem3.py

--- problem3.py
+++ problem3.py
@@ -8,3 +8,16 @@ into another number.
 Example: The factors of 10 are 1, 2, 5, 10
 (2 points)
 """
+def factors(a):
+    List=[a]
+    for i in range (1,a):
+        if a%i==0:
+            List.append(i)
+        List.sort()
+    return List
+
+x=factors(12)
+print(x)
+
+y=factors(37)
+print(y)
