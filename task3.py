b0d8560b54c32ba6a84d437748850346eecb3278
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-08 12:04:12 -0700

    Update task3.py

--- task3.py
+++ task3.py
@@ -7,18 +7,3 @@ added together
 (2 points)
 """
 
-
-def perimeter(List):
-    a=0
-    count=len[List]
-    for i in range(0,count):
-        x=List[i]
-        y=x+a
-        a=y
-        return y
-
-q = perimeter([5,2,6])
-print(q)
-
-p= perimeter([9,8,6,5.5])
-print(p)
