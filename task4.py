7cecda4eb6917f0992f95ae76637d2863f5eae20
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-08 11:42:08 -0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -7,3 +7,14 @@ Return False if the number is not an integer
 (2 points)
 """
 
+def isInteger (a):
+    if float(a)%1==0:
+        return True
+    elif float(a)%1!=0:
+        return False
+
+x=isInteger(9.5)
+print(x)
+
+y=isInteger(-2)
+print(y)
