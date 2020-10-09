f4d72a859c2ac942fc338b5a96f984d1cf0f1923
realllllyx18 <71406802+realllllyx18@users.noreply.github.com>
2020-10-08 18:17:24 -0700

    Update problem2.py

--- problem2.py
+++ problem2.py
@@ -7,17 +7,15 @@ Return value is the distance between the (x,y) coordinates.
 Note that the coordinates should be signed (positive or negative) floats
 (2 points)
 """
-iimport math
+import math
 
-def distance(tuples1,tuples2):
-    
-    answer=round(math.sqrt((float(tuples1[0])-float(tuples2[0]))**2+(float(tuples1[1])-float(tuples2[1]))**2,3)
+def distance(tup1,tup2):
+
+    answer = round(math.sqrt((float(tup1[0])-float(tup2[0]))**2 + (float(tup1[1])-float(tup2[1]))**2),3)
     return answer
 
-
-x = distance((2,4) , (6,3))
+x = distance((2,4),(6,3))
 print(x)
-
-y = distance((-3,2.2) , (1,2)))
+y = distance((-3,2.2),(1,2))
 print(y)
 
